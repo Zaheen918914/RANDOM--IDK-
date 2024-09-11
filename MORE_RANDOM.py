fb364fee70b6dbc8b34f27b26902f34c525b5bef
Zaheen918914 <zaheensirajee@gmail.com>
2024-09-11 22:08:57 +0600

    RANDOM

---
+++ MORE_RANDOM.py
@@ -0,0 +1,33 @@
+import random
+
+#RANDOM INT
+
+
+my_num = random.randint(1,10)
+
+#RANDOM FlOAT
+
+float = random.random()
+#print(float)
+
+#RANDOM ALPHABET
+
+text = 'Bangladesh'
+
+alpha = random.choice(text)
+#print(alpha)
+
+#RANDOM TEXT 
+
+#LIST 
+
+fruit = ['mango','apple','watermelon','grape','jackfruit']
+
+#print(random.choice(fruit))
+
+#TOSS 
+
+toss = ['head','tail']
+
+print(random.choice(toss))
+
